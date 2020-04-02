38ecc5b30dc9e755cf63f1c1a05d3467334ff7f4
Satyaki233 <satyakidesarkar2017@gmail.com>
2020-04-02 05:59:34 -0400

    Updates

---
+++ precache-manifest.7a8dcae7d73240b47f5e4860bc61658e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cc3c243976849c17ada83d446d1909",
+    "url": "/exam/index.html"
+  },
+  {
+    "revision": "5b3c38621f8aa3ffa8a6",
+    "url": "/exam/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e109dc83d06f03108d3c",
+    "url": "/exam/static/js/2.bdc5d016.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/exam/static/js/2.bdc5d016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b3c38621f8aa3ffa8a6",
+    "url": "/exam/static/js/main.2222800e.chunk.js"
+  },
+  {
+    "revision": "fdb4d94c42d100cdf799",
+    "url": "/exam/static/js/runtime-main.8abe9104.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/exam/static/media/logo.5d5d9eef.svg"
+  }
+]);
